e1d60817cc00a5d72232f79bbedece082c873a63
ppwater01 <69420216+ppwater01@users.noreply.github.com>
2025-01-08 11:23:42 +0900

    Update main.py
    change buttons

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ st.write('안녕하세요, 저는 --- 입니다')
 st.write('저의 이메일 주소는 undefined@null.com')
 
 st.button("Reset", type="primary")
-if st.button("Say hello"):
+if st.button("난수 생성"):
     st.write(random.randint(1, 1000))
 else:
     st.write("Goodbye")
-
-if st.button("Aloha", type="tertiary"):
-    st.write("Ciao")
